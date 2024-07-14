08641d36462c8e9300a03c078b78b03fd52e3b35
M7moodAyman96 <mahmood.ayman96@gmail.com>
2024-07-14 09:41:56 +0300

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Apple on 14/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
